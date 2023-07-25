5c792c3278951a11fac1df5332b7d19341a93b0d
bagelbomb <bagelbomb@gmail.com>
2023-07-24 17:57:12 -0700

    feat: add UI version and some styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,82 @@
+import { useState, useRef } from 'react';
+import words from './words.json';
 import './App.css';
 
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
+
 function App() {
+  const [storyText, setStoryText] = useState('');
+  const [storyList, setStoryList] = useState([]);
+  const inputRef = useRef();
+
+  const handleStartStory = () => {
+    setStoryText(capitalize(getRandomWord()));
+    inputRef.current.focus();
+  };
+
+  const handleTextChange = e => {
+    setStoryText(e.target.value);
+  };
+
+  const handleEnter = () => {
+    const lastLetter = storyText[storyText.length - 1];
+
+    if ('.!?'.includes(lastLetter)) {
+      setStoryList(prev => [...prev, storyText]);
+      setStoryText('');
+    } else {
+      setStoryText(
+        prev => prev + (lastLetter === ' ' ? '' : ' ') + getRandomWord()
+      );
+    }
+
+    inputRef.current.focus();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        What's up, world?
-      </header>
+    <div className='app'>
+      <h1 className='app-title'>X Word Story</h1>
+      <div className='instructions'>
+        <p>Write a single-sentence story with me, word by word!</p>
+        <p>You can start the story or have me start it.</p>
+        <p>Press Enter/Return after entering a word.</p>
+        <p>
+          End the story with a period (.), exclamation point (!), or question
+          mark (?).
+        </p>
+      </div>
+      <div className='story-input-div'>
+        <button className='start-button' onClick={handleStartStory}>
+          Start Story
+        </button>
+        <input
+          className='story-input'
+          type='text'
+          ref={inputRef}
+          value={storyText}
+          onChange={handleTextChange}
+          onKeyDown={e => e.key === 'Enter' && handleEnter()}
+        />
+        <button className='enter-button' onClick={handleEnter}>
+          &#8658;
+        </button>
+      </div>
+      <h2 className='stories-title'>Stories</h2>
+      <div className='stories-div'>
+        {storyList.map(
+          (
+            s // TODO: add keys
+          ) => (
+            <p>{s}</p>
+          )
+        )}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -10,3 +9,9 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// TODO: prevent changing previous parts of the story
+// TODO: save stories to local storage
+// TODO: clear stories
+// TODO: delete stories
+// TODO: add utils with common functions(?)
